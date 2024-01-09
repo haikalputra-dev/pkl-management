8c759a17d888546ae0e3e6e5e385a9bcaade6de0
piqieBaru <piqieaja@gmail.com>
2024-01-09 22:00:27 +0700

    pp

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\StorageAttributes;
+use Illuminate\Support\Facades\DB;
+use App\Models\Absensi;
+use Illuminate\Contracts\Auth\Guard;
 
 class UserController extends Controller
 {
     //
     public function UserDashboard()
     {
-        return view('user.index');
+        $hariini = date("Y-m-d");
+        $id = Auth::guard('web')->user()->id;
+        $cek = DB::table('absensi')->where('tanggal', $hariini)->where('id', $id)->count();
+        $presensihariini = DB::table('absensi')->where('tanggal', $hariini)->where('id', $id)->first();
+
+        return view('user.index', compact('cek', 'presensihariini'));
     }
     public function UserLogout(Request $request)
     { {
@@ -22,5 +32,57 @@ class UserController extends Controller
 
             return redirect('/');
         }
+    }
+
+    public function UserPresensi()
+    {
+
+        return view('user.presensi');
+    }
+    public function UserPresensis()
+    {
+        $id = Auth::guard('web')->user()->id;
+        $bulanini = date('m');
+        $tahunini = date('Y');
+        $datapresensi = DB::table('absensi')->join('users', 'absensi.user_id','=','users.id')->where('user_id',$id)->whereRaw('MONTH(tanggal)="' . $bulanini . '"')->whereRaw('YEAR(tanggal)="' . $tahunini . '"')->orderBy('tanggal')->select('absensi.*','users.name')->get();
+        return view('user.data_presensi', compact('datapresensi'));
+    }
+
+    public function store(Request $request)
+    {
+        $id = Auth::guard('web')->User()->id;
+        $tanggal = date("Y-m-d");
+        $jam = date("H:i:s");
+        $image = $request->image;
+        $folderPath = "public/upload/absensi/";
+        $image_part = explode(";base64", $image);
+        $image_base64 = base64_decode($image_part[1]);
+        $filename = uniqid() . ".png";
+        $file = $folderPath . $filename;
+        $data = [
+            'id' => $id,
+            'user_id' => $id,
+            'tanggal' => $tanggal,
+            'jam_masuk' => $jam,
+            'foto_in' => $filename
+        ];
+        $simpan = DB::table('absensi')->insert($data);
+        if ($simpan) {
+            echo 0;
+            Storage::put($file, $image_base64);
+        } else {
+            echo 1;
+        }
+    }
+
+    public function UserJurnal()
+    {
+
+        return view('user.jurnal');
+    }
+    public function UserMateri()
+    {
+
+        return view('user.materi');
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,61 +3,53 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
 
+<style>
+    .webcame-capture,
+    .webcame-capture video {
+        display: inline-block;
+        width: 100% !important;
+        margin: auto;
+        height: auto !important;
+        border-radius: 15px;
+    }
+</style>
 
 <div class="page-content">
 
     <nav class="page-breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">njing</a></li>
+            <li class="breadcrumb-item"><a href="#">dashboard</a></li>
             <li class="breadcrumb-item active" aria-current="page">Feather Icons</li>
         </ol>
     </nav>
+
 
     <div class="row">
         <div class="col-md-12 stretch-card">
             <div class="card">
                 <div class="card-body">
                     <h6 class="card-title">Feather Icons</h6>
-                    <p class="text-muted mb-3">Visit the <a href="https://feathericons.com/" target="_blank"> Official Feather Icons Documentation </a>.</p>
-                    <p class="text-muted mb-3">Usage example : &lt;i data-feather="star"&gt;&lt;/i&gt;</p>
-                    <div class="container">
-                        <form method="POST" action="{{ route('webcam.capture') }}">
-                            @csrf
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div id="my_camera"></div>
-                                    <br />
-                                    <input type=button value="Take Snapshot" onClick="take_snapshot()">
-                                    <input type="hidden" name="image" class="image-tag">
-                                </div>
-                                <div class="col-md-6">
-                                    <div id="results">Your captured image will appear here...</div>
-                                </div>
-                                <div class="col-md-12 text-center">
-                                    <br />
-                                    <button class="btn btn-success">Submit</button>
-                                </div>
-                            </div>
-                        </form>
+                    @if($cek > 0)
+
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Terimakash</h5>
+                            <p class="card-text mb-3">Anda telah absen hari ini</p>
+                        </div>
                     </div>
 
-                    <script language="JavaScript">
-                        Webcam.set({
-                            width: 490,
-                            height: 350,
-                            image_format: 'jpeg',
-                            jpeg_quality: 90
-                        });
 
-                        Webcam.attach('#my_camera');
+                    @else
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Absensi</h5>
+                            <p class="card-text mb-3">Anda belom absensi hari ini.</p>
+                            <a href="{{route('user.Presensi')}}" class="btn btn-primary">Silahkan silahkan Absensi</a>
+                        </div>
+                    </div>
 
-                        function take_snapshot() {
-                            Webcam.snap(function(data_uri) {
-                                $(".image-tag").val(data_uri);
-                                document.getElementById('results').innerHTML = '<img src="' + data_uri + '"/>';
-                            });
-                        }
-                    </script>
+                    @endif
                 </div>
             </div>
         </div>
